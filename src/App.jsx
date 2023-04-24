c1e74b324494a1fc4544331e10352ad6c8bd6f6a
Badriddin <frozeshkh@gmail.com>
2023-04-24 17:51:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+
+import useLocalStorage from './hooks/useLocalStorage'
+import CustomForm from '/src/components/CustomForm'
+import EditForm from '/src/components/EditForm'
+import TaskList from '/src/components/TaskList'
+
+function App() {
+    const [tasks, setTasks] = useLocalStorage('react-todo.tasks', []);
+    const [previousFocusEl, setPreviousFocusEl] = useState(null);
+    const [editedTask, setEditedTask] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const addTask = (task) => {
+        setTasks(prevState => [...prevState, task])
+    }
+
+    const deleteTask = (id) => {
+        setTasks(prevState => prevState.filter(t => t.id !== id));
+    }
+
+    const toggleTask = (id) => {
+        setTasks(prevState => prevState.map(t => (
+            t.id === id
+                ? { ...t, checked: !t.checked }
+                : t
+        )))
+    }
+
+    const updateTask = (task) => {
+        setTasks(prevState => prevState.map(t => (
+            t.id === task.id
+                ? { ...t, name: task.name }
+                : t
+        )))
+        closeEditMode();
+    }
+
+    const closeEditMode = () => {
+        setIsEditing(false);
+        previousFocusEl.focus();
+    }
+
+    const enterEditMode = (task) => {
+        setEditedTask(task);
+        setIsEditing(true);
+        setPreviousFocusEl(document.activeElement);
+    }
+
+    return (
+        <div className="container">
+            <header>
+                <h1>Maybe Admin Panel</h1>
+            </header>
+            {
+                isEditing && (
+                    <EditForm
+                        editedTask={editedTask}
+                        updateTask={updateTask}
+                        closeEditMode={closeEditMode}
+                    />
+                )
+            }
+            <CustomForm addTask={addTask} />
+            {tasks && (
+                <TaskList
+                    tasks={tasks}
+                    deleteTask={deleteTask}
+                    toggleTask={toggleTask}
+                    enterEditMode={enterEditMode}
+                />
+            )}
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,20 @@
+import TaskItem from '/src/Components/TaskItem';
+import styles from '/src/Components/TaskList.module.css';
+
+const TaskList = ({ tasks, deleteTask, toggleTask, enterEditMode }) => {
+  return (
+    <ul className={styles.tasks}>
+      {tasks.sort((a, b) => b.id - a.id).map(task => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          deleteTask={deleteTask}
+          toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
+        />
+      ))
+      }
+    </ul>
+  )
+}
+export default TaskList
